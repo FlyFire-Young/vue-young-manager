50f75341be372046d757f7fd1e3b9e05870001fb
FlyFire-Young <mr_liuy01@163.com>
2017-09-13 12:44:00 +0800

    Young

---
+++ src/components/common/Header.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="header">
+        <div class="logo">
+            <img class="logo-img-style" src="../../assets/y.png"/>
+            <span>Young后台管理系统</span>
+        </div>
+        <div class="user-info">
+            <el-dropdown trigger="click" @command="handleCommand">
+                <span class="el-dropdown-link">
+                    <img class="user-logo" src="../../assets/y.png">
+                    {{username}}
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item command="loginout">退出</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                name: 'linxin'
+            }
+        },
+        computed:{
+            username(){
+                let username = localStorage.getItem('ms_username');
+                return username ? username : this.name;
+            }
+        },
+        methods:{
+            handleCommand(command) {
+                if(command == 'loginout'){
+                    localStorage.removeItem('ms_username')
+                    this.$router.push('/login');
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+    .header {
+        position: relative;
+        box-sizing: border-box;
+        width: 100%;
+        height: 70px;
+        font-size: 22px;
+        line-height: 70px;
+        color: #fff;
+    }
+    .header .logo{
+        float: left;
+        width:250px;
+        text-align: center;
+    }
+    .user-info {
+        float: right;
+        padding-right: 50px;
+        font-size: 16px;
+        color: #fff;
+    }
+    .user-info .el-dropdown-link{
+        position: relative;
+        display: inline-block;
+        padding-left: 50px;
+        color: #fff;
+        cursor: pointer;
+        vertical-align: middle;
+    }
+    .user-info .user-logo{
+        position: absolute;
+        left:0;
+        top:15px;
+        width:40px;
+        height:40px;
+        border-radius: 50%;
+    }
+
+    .logo-img-style{
+        width: 40px;
+        height: 40px;
+        top: 15px;
+        position: relative;
+    }
+    .el-dropdown-menu__item{
+        text-align: center;
+    }
+</style>
